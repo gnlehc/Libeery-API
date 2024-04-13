e2c7eae0fbac97d91a75eed672cfa12610c2d3d5
gnlehc <chelseang.cn@gmail.com>
2024-04-13 12:41:21 +0700

    fix: env

--- database/PostgresDB.go
+++ database/PostgresDB.go
@@ -23,11 +23,11 @@ func DatabaseConnection() (err error) {
 	dbPort := os.Getenv("DB_PORT")
 	dbDatabase := os.Getenv("DB_DATABASE")
 
-	fmt.Println(dbUsername)
-	fmt.Println(dbDatabase)
-	fmt.Println(dbHost)
-	fmt.Println(dbPort)
-	fmt.Println(dbPassword)
+	// fmt.Println(dbUsername)
+	// fmt.Println(dbDatabase)
+	// fmt.Println(dbHost)
+	// fmt.Println(dbPort)
+	// fmt.Println(dbPassword)
 
 	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
 		dbUsername,
