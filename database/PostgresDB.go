3ec052a437f42167889d82b8b0f32d40be6d5456
gnlehc <chelseang.cn@gmail.com>
2024-04-13 16:17:51 +0700

    fix: env on dev

--- database/PostgresDB.go
+++ database/PostgresDB.go
@@ -13,22 +13,24 @@ import (
 var GlobalDB *gorm.DB
 
 func DatabaseConnection() (err error) {
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error loading .env file")
+	if _, exists := os.LookupEnv("RAILWAY_ENVIRONMENT"); exists == false {
+		if err := godotenv.Load(); err != nil {
+			log.Fatal("error loading .env file:", err)
+		}
 	}
 	// env live railway
-	dbUsername := os.Getenv("DB_USERNAME")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
-	dbDatabase := os.Getenv("DB_DATABASE")
+	dbUsername := os.Getenv("PGUSER")
+	dbPassword := os.Getenv("PGPASSWORD")
+	dbHost := os.Getenv("PGHOST")
+	dbPort := os.Getenv("PGPORT")
+	dbDatabase := os.Getenv("PGDATABASE")
 
-	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		dbHost,
 		dbUsername,
 		dbPassword,
-		dbHost,
-		dbPort,
-		dbDatabase)
+		dbDatabase,
+		dbPort)
 
 	GlobalDB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
