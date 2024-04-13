9265dd2983ed8176e42543bd5690ef8ee39c5efe
gnlehc <chelseang.cn@gmail.com>
2024-04-13 13:43:16 +0700

    fix: .env file

--- database/PostgresDB.go
+++ database/PostgresDB.go
@@ -16,24 +16,25 @@ func DatabaseConnection() (err error) {
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	// env live railway
-	// dbUsername := os.Getenv("DB_USERNAME")
-	// dbPassword := os.Getenv("DB_PASSWORD")
-	// dbHost := os.Getenv("DB_HOST")
-	// dbPort := os.Getenv("DB_PORT")
-	// dbDatabase := os.Getenv("DB_DATABASE")
-	dbUsername := os.Getenv("PGHOST")
-	dbPassword := os.Getenv("PGUSER")
-	dbHost := os.Getenv("PGDATABASE")
-	dbPort := os.Getenv("PGPASSWORD")
-	dbDatabase := os.Getenv("PGPORT")
 
-	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
+	dbHost := os.Getenv("PGHOST")
+	dbUsername := os.Getenv("PGUSER")
+	dbPassword := os.Getenv("PGPASSWORD")
+	dbDatabase := os.Getenv("PGDATABASE")
+	dbPort := os.Getenv("PGPORT")
+
+	fmt.Println("Database host:", dbHost)
+	fmt.Println("Database username:", dbUsername)
+	fmt.Println("Database password:", dbPassword)
+	fmt.Println("Database database:", dbDatabase)
+	fmt.Println("Database port:", dbPort)
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		dbHost,
 		dbUsername,
 		dbPassword,
-		dbHost,
-		dbPort,
-		dbDatabase)
+		dbDatabase,
+		dbPort)
 
 	GlobalDB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
